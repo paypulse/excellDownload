531cfe15b26aadf9f170fa4701b8d3a1a804308f
paypulse <sourcedev5@gmail.com>
2022-11-22 16:39:32 +0900

    대용량 excel download example

---
+++ src/main/java/com/example/sxssfworkbook/SxssfWorkbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.sxssfworkbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SxssfWorkbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SxssfWorkbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sxssfworkbook/SxssfWorkbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sxssfworkbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SxssfWorkbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
